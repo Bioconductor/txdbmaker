402f4e43411b4ded54e364ef5667d5a609ed5170
Hervé Pagès <hpages.on.github@gmail.com>
2023-10-27 22:55:39 -0700

    version 0.0.3

---
+++ inst/unitTests/test_makeTxDbFromUCSC.R
@@ -0,0 +1,16 @@
+###
+
+if (FALSE) {
+test_makeTxDbFromUCSC <- function()
+{
+    txdb_file <- system.file("extdata",
+                             "sacCer2_sgdGene_txdb.sqlite",
+                             package="txdbmaker")
+    target_txdb <- loadDb(txdb_file)
+
+    current_txdb <- makeTxDbFromUCSC("sacCer2", "sgdGene")
+
+    ok <- GenomicFeatures:::compareTxDbs(target_txdb, current_txdb)
+    checkTrue(ok)
+}
+}
---
+++ tests/run_unitTests.R
@@ -0,0 +1,2 @@
+require("txdbmaker") || stop("unable to load txdbmaker package")
+txdbmaker:::.test()
